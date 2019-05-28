d61aa416f3f4b582c79b7bb947d2d8d55e2ff701
Kiraow188 <17010368@myrp.edu.sg>
2019-05-28 12:09:27 +0800

    1

--- app/src/main/java/sg/edu/rp/c347/demonotification/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demonotification/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     int requestCode = 123;
     int notificationID = 888;
-    Button btnNotify1, btnNotify2;
+    Button btnNotify1, btnNotify2, btnNotify3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +84,40 @@ public class MainActivity extends AppCompatActivity {
                 builder.setSmallIcon(android.R.drawable.btn_star_big_off);
                 builder.setContentIntent(pIntent);
                 builder.setStyle(bigText);
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
+
+        btnNotify3 = findViewById(R.id.btnNotify3);
+
+        btnNotify3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+                    channel.setDescription("This is the default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.sentosa);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Welcome to Sentosa!");
+                builder.setContentText("Singapore's premier island getaway");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(picture));
                 builder.setAutoCancel(true);
 
                 Notification n = builder.build();
